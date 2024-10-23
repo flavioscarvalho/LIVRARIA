863a41367722279a67af5010ca812715ce1b9ac9
Flavio Carvalho <professorflaviosoares@gmail.com>
2024-10-23 20:47:58 -0300

    Update database settings to PostgreSQL and apply migrations

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -76,11 +76,11 @@ STORAGES = {
 # Database configuration, using environment variables
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("SQL_DATABASE", "bookstore_db"),
+        "USER": os.environ.get("SQL_USER", "bookstore"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "bookstore_password"),
+        "HOST": os.environ.get("SQL_HOST", "db"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
