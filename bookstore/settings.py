31e4db2dababd219a7143babe6d1230415e881b4
Flavio Carvalho <professorflaviosoares@gmail.com>
2024-10-25 16:53:51 -0300

    Aplicando alterações ao workflow e settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url  # Import dj_database_url para manipular a URL do banco de dados
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,16 +10,15 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # Hosts configuration
-# Alterações realizadas em 23/10/2024 para testes
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'ebac-livraria-api-a99a1687e9d4.herokuapp.com']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 # Application definition
 INSTALLED_APPS = [
-    "django.contrib.admin",  # Admin app
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.sessions",  # Sessions for admin and other functionalities
-    "django.contrib.messages",  # MessageMiddleware depends on this app
+    "django.contrib.sessions",
+    "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
     "rest_framework",
@@ -29,15 +29,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",  # Reforça configurações de segurança
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # Gerenciamento de arquivos estáticos
-    "django.contrib.sessions.middleware.SessionMiddleware",  # Habilita o uso de sessões
-    "django.middleware.common.CommonMiddleware",  # Middleware comum para várias funcionalidades
-    "django.middleware.csrf.CsrfViewMiddleware",  # Proteção contra CSRF
-    "django.contrib.auth.middleware.AuthenticationMiddleware",  # Gerencia a autenticação do usuário
-    "django.contrib.messages.middleware.MessageMiddleware",  # Gerencia mensagens no sistema
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",  # Protege contra ataques de clickjacking
-    "debug_toolbar.middleware.DebugToolbarMiddleware",  # Django Debug Toolbar
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "bookstore.urls"
@@ -45,7 +45,7 @@ ROOT_URLCONF = "bookstore.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],  # Add additional template directories if needed
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -61,7 +61,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "bookstore.wsgi.application"
 
 # Static files configuration
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Diretório onde todos os arquivos estáticos serão coletados pelo comando 'collectstatic'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = "static/"
 
 STORAGES = {
@@ -73,16 +73,12 @@ STORAGES = {
     },
 }
 
-# Database configuration, using environment variables
+# Database configuration using environment variables and DATABASE_URL
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("SQL_DATABASE", "bookstore_db"),
-        "USER": os.environ.get("SQL_USER", "bookstore"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "bookstore_password"),
-        "HOST": os.environ.get("SQL_HOST", "db"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
-    }
+    "default": dj_database_url.config(
+        default=f"postgres://{os.environ.get('SQL_USER', 'bookstore')}:{os.environ.get('SQL_PASSWORD', 'bookstore_password')}@{os.environ.get('SQL_HOST', 'localhost')}:{os.environ.get('SQL_PORT', '5432')}/{os.environ.get('SQL_DATABASE', 'bookstore_db')}",
+        conn_max_age=600
+    )
 }
 
 # Password validation
@@ -118,8 +114,135 @@ REST_FRAMEWORK = {
     ],
 }
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://ebac-livraria-api-a99a1687e9d4.herokuapp.com',
-]
+
+
+
+
+
+# import os
+# from pathlib import Path
+
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# # SECRET_KEY and DEBUG from environment variables
+# SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+# DEBUG = int(os.environ.get("DEBUG", default=0))
+
+# # Hosts configuration
+# # Alterações realizadas em 23/10/2024 para testes
+# ALLOWED_HOSTS = ['localhost','127.0.0.1', 'ebac-livraria-api-a99a1687e9d4.herokuapp.com']
+
+# # Application definition
+# INSTALLED_APPS = [
+#     "django.contrib.admin",  # Admin app
+#     "django.contrib.auth",
+#     "django.contrib.contenttypes",
+#     "django.contrib.sessions",  # Sessions for admin and other functionalities
+#     "django.contrib.messages",  # MessageMiddleware depends on this app
+#     "django.contrib.staticfiles",
+#     "django_extensions",
+#     "rest_framework",
+#     "rest_framework.authtoken",
+#     "order",
+#     "product",
+#     "debug_toolbar",
+# ]
+
+# MIDDLEWARE = [
+#     "django.middleware.security.SecurityMiddleware",  # Reforça configurações de segurança
+#     "whitenoise.middleware.WhiteNoiseMiddleware",  # Gerenciamento de arquivos estáticos
+#     "django.contrib.sessions.middleware.SessionMiddleware",  # Habilita o uso de sessões
+#     "django.middleware.common.CommonMiddleware",  # Middleware comum para várias funcionalidades
+#     "django.middleware.csrf.CsrfViewMiddleware",  # Proteção contra CSRF
+#     "django.contrib.auth.middleware.AuthenticationMiddleware",  # Gerencia a autenticação do usuário
+#     "django.contrib.messages.middleware.MessageMiddleware",  # Gerencia mensagens no sistema
+#     "django.middleware.clickjacking.XFrameOptionsMiddleware",  # Protege contra ataques de clickjacking
+#     "debug_toolbar.middleware.DebugToolbarMiddleware",  # Django Debug Toolbar
+# ]
+
+# ROOT_URLCONF = "bookstore.urls"
+
+# TEMPLATES = [
+#     {
+#         "BACKEND": "django.template.backends.django.DjangoTemplates",
+#         "DIRS": [],  # Add additional template directories if needed
+#         "APP_DIRS": True,
+#         "OPTIONS": {
+#             "context_processors": [
+#                 "django.template.context_processors.debug",
+#                 "django.template.context_processors.request",
+#                 "django.contrib.auth.context_processors.auth",
+#                 "django.contrib.messages.context_processors.messages",
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = "bookstore.wsgi.application"
+
+# # Static files configuration
+# STATIC_ROOT = BASE_DIR / 'staticfiles'  # Diretório onde todos os arquivos estáticos serão coletados pelo comando 'collectstatic'
+# STATIC_URL = "static/"
+
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "django.core.files.storage.FileSystemStorage",
+#     },
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
+
+# # Database configuration, using environment variables
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("SQL_DATABASE", "bookstore_db"),
+#         "USER": os.environ.get("SQL_USER", "bookstore"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD", "bookstore_password"),
+#         "HOST": os.environ.get("SQL_HOST", "db"),
+#         "PORT": os.environ.get("SQL_PORT", "5432"),
+#     }
+# }
+
+# # Password validation
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+#     },
+#     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+#     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+#     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+# ]
+
+# # Internationalization
+# LANGUAGE_CODE = "en-us"
+# TIME_ZONE = "UTC"
+# USE_I18N = True
+# USE_TZ = True
+
+# # Default primary key field type
+# DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# # Django Debug Toolbar
+# INTERNAL_IPS = ["127.0.0.1"]
+
+# # Django REST Framework settings
+# REST_FRAMEWORK = {
+#     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+#     "PAGE_SIZE": 5,
+#     "DEFAULT_AUTHENTICATION_CLASSES": [
+#         "rest_framework.authentication.BasicAuthentication",
+#         "rest_framework.authentication.SessionAuthentication",
+#         "rest_framework.authentication.TokenAuthentication",
+#     ],
+# }
+
+
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://ebac-livraria-api-a99a1687e9d4.herokuapp.com',
+# ]
 
